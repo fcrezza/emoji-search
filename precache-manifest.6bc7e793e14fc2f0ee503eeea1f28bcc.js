e3742f1415becb67bfadd1ff959216aa90454bb3
Anang Fachreza <fachrezanang@gmail.com>
2019-06-17 19:44:51 +0700

    Updates

---
+++ precache-manifest.6bc7e793e14fc2f0ee503eeea1f28bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e08bd3116a2c7a8e093f85e2f02692",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "56aa9be7a5204cfbcec4",
+    "url": "/emoji-search/static/css/2.69e88859.chunk.css"
+  },
+  {
+    "revision": "36c5eeb6b6853c745cc8",
+    "url": "/emoji-search/static/css/main.524b7e9b.chunk.css"
+  },
+  {
+    "revision": "56aa9be7a5204cfbcec4",
+    "url": "/emoji-search/static/js/2.8629dcae.chunk.js"
+  },
+  {
+    "revision": "36c5eeb6b6853c745cc8",
+    "url": "/emoji-search/static/js/main.8d2bf06f.chunk.js"
+  },
+  {
+    "revision": "0b9b21e1bdea238a126f",
+    "url": "/emoji-search/static/js/runtime~main.4b14439d.js"
+  }
+]);
